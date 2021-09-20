920625c7cc333c1eafc5064218eda430ab9fadf3
Chuck D <c.a.dawkins@outlook.com>
2021-09-20 15:39:47 -0400

    changes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,6 +5,7 @@ const sequelize = require("../../config/connection");
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  // find all categories
   Category.findAll({
     order: [["id", "ASC"]],
     attributes: [
@@ -27,7 +28,6 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
   Category.findOne({
     where: {
       id: req.params.id
@@ -51,6 +51,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
+  // create a new category
   Category.create({
     category_name: req.body.category_name
   })
@@ -62,6 +63,7 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
+  // update a category by its `id` value
   Category.update(
     {
       category_name: req.body.category_name
@@ -86,6 +88,7 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
   Category.destroy({
     where: {
       id: req.params.id
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,6 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
   Tag.findAll({
     attributes: [
       "id",
@@ -34,7 +33,6 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
   Tag.findOne({
     where: {
       id: req.params.id
